b09649a9d2cadb927a356be54d0bee555f4dbd6f
RobAmorim <falandocomrobson@gmail.com>
2017-02-07 11:47:09 -0200

    Add files via upload

---
+++ Sensor Reading/Sharp - Infrared/To Read_Sharp_IR.ino
@@ -0,0 +1,38 @@
+
+// CONFIGURAÇÕES SHARPS
+#include <SharpIR.h>
+#define irD A9
+#define irE A8
+#define model 1080
+SharpIR sharpDirSensor(irD, 25, 93, model);
+SharpIR sharpEsqSensor(irE, 25, 93, model);
+unsigned int sharpDirValue = 0;
+unsigned int sharpEsqValue = 0;
+
+void LeituraSharps();
+
+void setup() {
+ Serial.begin(9600);
+
+}
+
+void loop() {
+
+LeituraSharps; 
+
+Serial.print("Distância Dir:");
+Serial.print(sharpDirValue);
+
+
+Serial.print("Distância Esq:");
+Serial.println(sharpEsqValue);
+
+delay(400); 
+
+
+}
+
+void LeituraSharps() {
+  sharpEsqValue = sharpEsqSensor.distance();
+  sharpDirValue = sharpDirSensor.distance();
+}

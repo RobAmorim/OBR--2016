e8f22d183d656f35e72e74e5505511345cc67ae1
RobAmorim <falandocomrobson@gmail.com>
2017-02-11 08:41:03 -0200

    Add files via upload

---
+++ Official Robot Programming/Void_RAMP.ino
@@ -0,0 +1,77 @@
+
+/*----------------------------------------------------------------------------
+ * Oficial championsship Programing (function -> void Ramp) 
+ * ---------------------------------------------------------------------------
+ * $Author Roboson Vincius Amorim Silva
+ * $Date 2016-07 (July 2015) $
+ * ---------------------------------------------------------------------------
+ */
+
+void Ramp()
+
+{
+
+Robot.speed(240, 240);
+//======================================================================================================================
+                                                 //Basic - Line Following 
+//======================================================================================================================
+  ReadReflectance();
+ if((valueLeftLine > cutoffValue)&&(valueRightLine > cutoffValue)) //Central Sensors is in black
+  {
+   Robot.forward();     
+  }
+  
+ if((valueLeftLine < cutoffValue)&&(valueRightLine < cutoffValue)&&(valueLeftCorner < cutoffValue)&&  // ALL sensors is in White  
+  (valueRightCorner < cutoffValue)&&(valueLeftEx < cutoffValue)&&(valueRightEx < cutoffValue))
+  {
+    Robot.forward();   
+  }
+ 
+ if((valueLeftLine < cutoffValue)&&(valueRightLine > cutoffValue)) // Central sensor left is in whith and central sensor right is in black  
+ {
+  Robot.left(150, 150); 
+  }
+  
+ if((valueLeftLine > cutoffValue)&&(valueRightLine < cutoffValue)) // Central sensor left is in black and central sensor right is in white
+  {
+  Robot.right(150, 150); 
+  }
+//============================================================================================================================
+                                          //Corner and Extreme - Line following 
+//=======================================================================================================================
+    if((valueLeftLine < cutoffValue)&&(valueRightLine < cutoffValue)&&(valueLeftCorner > cutoffValue)&&  //All sensors is in whith and the sensor of left corner is in black 
+  (valueRightCorner < cutoffValue )&&(valueLeftEx < cutoffValue )&&(valueRightEx < cutoffValue ))
+  {
+  Robot.right(); 
+  }
+    if((valueLeftLine < cutoffValue)&&(valueRightLine < cutoffValue)&&(valueLeftCorner < cutoffValue)&&  // All sensors is in whith and the sensor of left extreme is in black
+  (valueRightCorner < cutoffValue)&&(valueLeftEx > cutoffValue)&&(valueRightEx < cutoffValue))
+  {
+   Robot.right(150); 
+  }
+  if((valueLeftLine < cutoffValue)&&(valueRightLine < cutoffValue)&&(valueLeftCorner > cutoffValue)&&  // All sensors is in whith and the sensor of left extreme and 
+  (valueRightCorner < cutoffValue)&&(valueLeftEx > cutoffValue)&&(valueRightEx < cutoffValue))         // left corner  is in black
+  {
+   Robot.right(150); 
+  }
+  
+  if((valueLeftLine < cutoffValue)&&(valueRightLine < cutoffValue)&&(valueLeftCorner < cutoffValue)&&  //All sensors is in whith and the sensor of right corner is in black 
+  (valueRightCorner > cutoffValue)&&(valueLeftEx < cutoffValue)&&(valueRightEx < cutoffValue))
+  {
+    Robot.left(150);
+  }
+  
+  if((valueLeftLine < cutoffValue)&&(valueRightLine < cutoffValue)&&(valueLeftCorner < cutoffValue)&&  // All sensors is in whith and the sensor of right extreme is in black
+  (valueRightCorner < cutoffValue)&&(valueLeftEx < cutoffValue)&&(valueRightEx > cutoffValue))
+  {
+    Robot.left(150);
+  }
+  if((valueLeftLine < cutoffValue)&&(valueRightLine < cutoffValue)&&(valueLeftCorner < cutoffValue)&&  // All sensors is in whith and the sensor of right extreme and 
+  (valueRightCorner > cutoffValue)&&(valueLeftEx < cutoffValue)&&(valueRightEx > cutoffValue))         // right corner  is in black
+  {
+    Robot.left(150);  
+  }
+//=================================================================================================================================
+
+
+}
---
+++ Official Robot Programming/Void_TurnLeft_TurnRight.ino
@@ -0,0 +1,85 @@
+
+/*----------------------------------------------------------------------------
+ * Oficial championsship Programing (function -> void Turn Right and Left with gyro) 
+ * ---------------------------------------------------------------------------
+ * $Author Roboson Vincius Amorim Silva
+ * $Date 2016-07 (July 2015) $
+ * ---------------------------------------------------------------------------
+ */
+
+void TurnRight() //Turn robot for right with gyro sensor 
+{
+     Robot.speed(230, 230);
+   resetGyro();
+  
+while(TotalDegrees > ninetyDegreeRight) //Turn right 
+  {  
+     AGRead();
+     TotalDegrees += GDegreesZ;
+     Robot.right();     
+  }
+    Robot.stop();
+    resetGyro();
+}
+
+void TurnLeft() //Turn robot for right with gyro sensor 
+
+{
+      Robot.speed(230, 230);
+      resetGyro();
+ while(TotalDegrees < ninetyDegreeLeft)  //Turn Left 
+   {
+      AGRead();
+      TotalDegrees += GDegreesZ;
+      Robot.left();
+   }
+    Robot.stop(); 
+    resetGyro();
+}
+//=========================================================================================
+
+void TurnLeftAdjust() //Adjust of curve in room 3 
+
+{
+      Robot.speed(230, 230);
+      resetGyro();
+ while(TotalDegrees < adjustLeft)  //turn left 
+   {
+      AGRead();
+      TotalDegrees += GDegreesZ;
+      Robot.left();
+   }
+    Robot.stop(); 
+    resetGyro();
+}
+
+void TurnTriRight() //Right curve in room 3 
+{
+     Robot.speed(230, 230);
+   resetGyro();
+  
+while(TotalDegrees > TriRight) // turn right 
+  {  
+     AGRead();
+     TotalDegrees += GDegreesZ;
+     Robot.right();     
+  }
+    Robot.stop();
+    resetGyro();
+}
+
+void TurnTriLeft() //Left curve in room 3  
+
+{
+      Robot.speed(230, 230);
+      resetGyro();
+ while(TotalDegrees < TriLeft)  //turn left 
+   {
+      AGRead();
+      TotalDegrees += GDegreesZ;
+      Robot.left();
+   }
+    Robot.stop(); 
+    resetGyro();
+}
+//=========================================================================================

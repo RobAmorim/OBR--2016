0282970345c000737f3278e8849c0c912b61fc23
Robson Vinicius Amorim Silva <falandocomrobson@gmail.com>
2017-02-22 11:32:12 -0300

    Update Two- Green-To Read.ino

--- Sensor Reading/Color Sensor- TCS3200/Two- Green-To Read.ino
+++ Sensor Reading/Color Sensor- TCS3200/Two- Green-To Read.ino
@@ -1,24 +1,30 @@
-// Programa : Detector de cores usando modulo TCS230  
-// Alteracoes e comentarios : Arduino e Cia  
-//  
-// Baseado no programa original de Martin Mason  
+
+/*----------------------------------------------------------------------------
+ * COlor Sensor -TCS3200 - TO READ 
+ * ---------------------------------------------------------------------------
+ * $Author Roboson Vincius Amorim Silva
+ * $Date 2016-08 (July 2016) $
+ * ---------------------------------------------------------------------------
+ */
+
+//=================================================
    
-//Pinos de conexao do modulo TCS230 DIREITO 
-#define Dir_s0  37
-#define Dir_s1  39  
-#define Dir_s2  41
-#define Dir_s3  43  
-#define Dir_out 27
-//Pinos de conexao do modulo TCS230 ESQUERDO 
-#define Esq_s0  36
+                                  
+#define Right_s0  37    //Connection Pins Color sensor - Right  
+#define Right_s1  39  
+#define Right_s2  41
+#define Right_s3  43  
+#define Right_out 27
+
+#define Esq_s0  36    //Connection Pins Color sensor - Left
 #define Esq_s1  38  
 #define Esq_s2  40
 #define Esq_s3  42 
 #define Esq_out 26
-//Variaveis que armazenam o valor das cores  
 
-int Dir_green = 0;  
-int Esq_green = 0;  
+
+int Right_green = 0;    // variable to store the value coming from the right sensor 
+int Left_green = 0;   // variable to store the value coming from the right sensor 
 
 void color(); 
 
@@ -28,40 +34,40 @@ void setup()
 
 {  
   
-  pinMode(Dir_s0, OUTPUT);  //PINOS DO SENSOR DE COR DA DIREITA
-  pinMode(Dir_s1, OUTPUT);  
-  pinMode(Dir_s2, OUTPUT);  
-  pinMode(Dir_s3, OUTPUT);  
-  pinMode(Dir_out, INPUT);  
+  pinMode(Right_s0, OUTPUT);  //Setting Pins Color sensor - Right
+  pinMode(Right_s1, OUTPUT);  
+  pinMode(Right_s2, OUTPUT);  
+  pinMode(Right_s3, OUTPUT);  
+  pinMode(Right_out, INPUT);  
 //================================================================
-  pinMode(Esq_s0, OUTPUT);  //PINOS DO SENSOR DE COR DA ESQUERDA  
-  pinMode(Esq_s1, OUTPUT);  
-  pinMode(Esq_s2, OUTPUT);  
-  pinMode(Esq_s3, OUTPUT);  
-  pinMode(Esq_out, INPUT);  
+  pinMode(Left_s0, OUTPUT);  //Setting Pins Color sensor - Left 
+  pinMode(Left_s1, OUTPUT);  
+  pinMode(Left_s2, OUTPUT);  
+  pinMode(Left_s3, OUTPUT);  
+  pinMode(Left_out, INPUT);  
  
   Serial.begin(9600);  
   //============================================================================   
-  digitalWrite(Dir_s0, HIGH);  //CONDIÇÃO DE FREQUÊNCIA DO SENSOR DE COR DIREITO
-  digitalWrite(Dir_s1, LOW);
+  digitalWrite(Right_s0, HIGH);  //Setting frequency color sensor - Right 
+  digitalWrite(Right_s1, LOW);
   //============================================================================
-  digitalWrite(Esq_s0, HIGH);  //CONDIÇÃO DE FREQUÊNCIA DO SENSOR DE COR ESQUERDO
-  digitalWrite(Esq_s1, LOW);
+  digitalWrite(Left_s0, HIGH); //Setting frequency color sensor - Left 
+  digitalWrite(Left_s1, LOW);
 
   
 }  
     
 void loop() 
 {  
-  color(); //Chama a rotina que le as cores  
+  color(); //Routine that read color sensor  
   
-  //Mostra no serial monitor os valores detectados  
+  //to Show  values of color sensor readed 
  //=================================================================================
-  Serial.print(" Verde Dir : ");  //PRINTA O VALOR LIDO PELO SENSOR DE COR DIREITO
-  Serial.print(Dir_green, DEC);  
+  Serial.print(" Verde Dir : ");  //Print of values readed - Color Sensor Right 
+  Serial.print(Right_green, DEC);  
  //================================================================================ 
-  Serial.print(" ======  Verde Esq: ");   //PRINTA O VALOR LIDO PELO SENSOR DE COR ESQUERDO 
-  Serial.println(Esq_green, DEC);  
+  Serial.print(" ======  Verde Esq: ");   //Print of values readed - Color Sensor Left  
+  Serial.println(Left_green, DEC);  
   //=================================================================================
   
   delay(200); 
@@ -69,17 +75,17 @@ void loop()
     
 void color()  
 {  
-  //Rotina que le o valor das cores  
+  //Routine that read color sensor  
   
- digitalWrite(Dir_s2, HIGH);  // CONDIÇÃO QUE ACIONA OS FOTOTRANSISTORS PARA VERDE DO DIREITO
- digitalWrite(Dir_s3, HIGH);   
+ digitalWrite(Right_s2, HIGH);  // CONDIÇÃO QUE ACIONA OS FOTOTRANSISTORS PARA VERDE DO DIREITO
+ digitalWrite(Right_s3, HIGH);   
 //===============================
- digitalWrite(Esq_s2, HIGH);  // CONDIÇÃO QUE ACIONA OS FOTOTRANSISTORS PARA VERDE DO ESQUERDO
- digitalWrite(Esq_s3, HIGH);
+ digitalWrite(Left_s2, HIGH);  // CONDIÇÃO QUE ACIONA OS FOTOTRANSISTORS PARA VERDE DO ESQUERDO
+ digitalWrite(Left_s3, HIGH);
    
  //count OUT, pGreen, GREEN  
  
-  Dir_green = pulseIn(Dir_out, digitalRead(Dir_out) == HIGH ? LOW : HIGH); 
-  Esq_green = pulseIn(Esq_out, digitalRead(Esq_out) == HIGH ? LOW : HIGH);   
+  Right_green = pulseIn(Right_out, digitalRead(Right_out) == HIGH ? LOW : HIGH); 
+  Left_green = pulseIn(Left_out, digitalRead(Left_out) == HIGH ? LOW : HIGH);   
 }
 

b832cf716a426a2400e0a358d09d32ba2b72dbc5
Robson Vinicius Amorim Silva <falandocomrobson@gmail.com>
2017-02-22 11:24:14 -0300

    Update Single- Green-To Read.ino

--- Sensor Reading/Color Sensor- TCS3200/Single- Green-To Read.ino
+++ Sensor Reading/Color Sensor- TCS3200/Single- Green-To Read.ino
@@ -1,17 +1,22 @@
-// Programa : Detector de cores usando modulo TCS230  
-// Alteracoes e comentarios : Arduino e Cia  
-//  
-// Baseado no programa original de Martin Mason  
-   
-//Pinos de conexao do modulo TCS230 
-#define Pino_s0 22 
-#define Pino_s1 23  
-#define Pino_s2 24  
-#define Pino_s3 25  
-#define Pino_out A14 
+
+/*----------------------------------------------------------------------------
+ * Color Sensor - TCS3200 - Reading 
+ * ---------------------------------------------------------------------------
+ * $Author Roboson Vincius Amorim Silva
+ * $Date 2016-08 (July 2016) $
+ * ---------------------------------------------------------------------------
+ */
+
+//=================================================
+                       
+#define Pin_s0 22    //Connections Pins of color sensor  TCS230 
+#define Pin_s1 23  
+#define Pin_s2 24  
+#define Pin_s3 25  
+#define Pin_out A14 
 
  
-int green = 0;  
+int green = 0;  // variable to store the value coming from the right sensor 
 
     
 void setup()  
@@ -19,7 +24,7 @@ void setup()
 
 {  
   
-  pinMode(Pino_s0, OUTPUT);  //PINOS DO SENSOR DE COR 
+  pinMode(Pino_s0, OUTPUT);  //Pin setting - color sensor 
   pinMode(Pino_s1, OUTPUT);  
   pinMode(Pino_s2, OUTPUT);  
   pinMode(Pino_s3, OUTPUT);  
@@ -27,7 +32,7 @@ void setup()
 
   Serial.begin(9600);  
   //============================================================================   
-  digitalWrite(Pino_s0, HIGH);  //CONDIÇÃO DE FREQUÊNCIA DO SENSOR DE COR DIREITO
+  digitalWrite(Pino_s0, HIGH);  //Frequency Setting - color sensor 
   digitalWrite(Pino_s1, LOW);
   //============================================================================
  
@@ -37,11 +42,11 @@ void setup()
     
 void loop() 
 {  
-  color(); //Chama a rotina que le as cores  
+  color(); //Reading color sensor 
   
-  //Mostra no serial monitor os valores detectados  
+  //To show values readed of color sensor 
  //=================================================================================
-  Serial.print(" Valor Cor : ");  //PRINTA O VALOR LIDO PELO SENSOR DE COR DIREITO
+  Serial.print(" Value Color : ");  //Print values readed of color sensor 
   Serial.println(green, DEC);  
  //================================================================================ 
  
@@ -51,10 +56,10 @@ void loop()
     
 void color()  
 {  
-  //Rotina que le o valor das cores  
+  //Routine that read color sensor  
   
- digitalWrite(Pino_s2, HIGH);  // CONDIÇÃO QUE ACIONA OS FOTOTRANSISTORS PARA VERDE DO DIREITO
- digitalWrite(Pino_s3, HIGH);   
+ digitalWrite(Pino_s2, Right);  // CONDIÇÃO QUE ACIONA OS FOTOTRANSISTORS PARA VERDE DO DIREITO
+ digitalWrite(Pino_s3, Right);   
 //===============================
 
  //count OUT, pGreen, GREEN  
